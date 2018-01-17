b1fcd7f9b498aeb7833fef4e205d61fcfdc458a8
alejovelez10 <alejovelez10@gmail.com>
2018-01-17 08:29:44 -0500

    prximamente

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :teams
   resources :services
   devise_for :users
-  root 'home#inicio'
+  root 'home#index'
   get 'home/inicio', as: "inicio"
   get 'servicios' , to: "home#servicios", as: "servicios"
   get 'experiencias' , to: "home#experiencias", as: "experiencias"
